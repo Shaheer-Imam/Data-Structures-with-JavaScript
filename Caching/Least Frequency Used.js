1cf9936d24059d0f0376adeff13047057c2d8099
Shaheer-Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2019-08-16 18:42:53 +0500

    Data Structures

---
+++ Caching/Least Frequency Used.js
@@ -0,0 +1,107 @@
+function LFUNode(key,value){
+	this.prev = null;
+	this.next = null;
+	this.key = key;
+	this.data = value;
+	this.freqCount = 1;
+}
+
+function LFUDoublyLinkedList(){
+	this.head = new LFUNode('buffer head',null);
+	this.tail = new LFUNode('buffer tail',null);
+	this.head.next = this.tail;
+	this.tail.next = this.head;
+	this.size = 0
+}
+
+function LFUCache(capacity){
+	this.keys = {};
+	this.freq = {};
+	this.capacity = capacity;
+	this.minFreq = 0;
+	this.size = 0;
+}
+
+LFUDoublyLinkedList.prototype.insertAtHead = function(node){
+	node.next = this.head.next;
+	this.head.next.prev = node;
+	this.head.next = node;
+	node.prev = this.head;
+	this.size++;
+}
+
+LFUDoublyLinkedList.prototype.removeAtTail = function(){
+	var oldTail = this.tail.prev;
+	var prev = this.tail.prev;
+	prev.prev.next = this.tail;
+	this.tail.pev = prev.prev;
+	this.size--;
+	return oldTail;
+}
+
+LFUDoublyLinkedList.prototype.removeNode = function(node){
+	node.prev.next = node.next;
+	node.next.prev = node.prev;
+	this.size--;
+}
+
+LFUCache.prototype.set = function(key, value){
+	var node = this.keys[key];
+	if(node == undefined){
+		node = new LFUNode(key, value);
+		this.keys[key] = node;
+		if(this.size != this.capacity){
+			if(this.freq[1] === undefined){
+				this.freq[1] = new LFUDoublyLinkedList();
+			}
+			this.freq[1].insertAtHead(node);
+			this.size++;
+		}
+		else{
+			var oldTail = this.freq[this.minFreq].removeAtTail();
+			delete this.keys[oldTail.key];
+
+			if(this.freq[1] === undefined){
+				this.freq[1] = new LFUDoublyLinkedList();
+			}
+			this.freq[1].insertAtHead(node);
+		}
+		this.minFreq = 1;
+	}
+	else{
+		var oldFreqCount = node.freqCount;
+		node.data = value;
+		node.freqCount++;
+		this.freq[oldFreqCount].removeNode(node);
+
+		if(this.freq[node.freqCount] == undefined){
+			this.freq[node.freqCount] = new LFUDoublyLinkedList();
+		}
+		this.freqCount[node.freqCount].insertAtHead(node);
+
+		if(oldFreqCount == this.minFreq && Object.keys(this.freq[oldFreqCount]).size == 0){
+			this.minFreq++;
+		}
+
+	}
+}
+
+LFUCache.prototype.get = function(key){
+	var node = this.keys[key];
+	if(node == undefined){
+		return null;
+	}
+	else{
+		var oldFreqCount = node.freqCount;
+		node.freqCount++;
+		this.freq[oldFreqCount].removeNode(node);
+		if(this.freq[node.freqCount] === undefined){
+			this.freq[node.freqCount] = new LFUDoublyLinkedList();
+		}
+		this.freq[node.freqCount].insertAtHead(node);
+		if(oldFreqCount == this.minFreq && Object.keys(this.freq[oldFreqCount]).length ==0){
+			this.minFreq++;
+		}
+		return node.data;
+	}
+}
