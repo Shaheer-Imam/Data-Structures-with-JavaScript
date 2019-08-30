500fd979b3214ede579fb5f6b19d6f553fc92562
Shaheer-Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2019-08-31 00:37:24 +0500

    Added functionalities of Binary Tree

---
+++ Tree/Lowest Common Ancestor.js
@@ -0,0 +1,47 @@
+/* Write a function to find the lowest common ancestor of two nodes in a given bin ary tree. 
+If the maximum of the two values is smaller than the current root, go left. If the minimum of the two values is bigger than the current root, go right. */ 
+function findLowestCommonAncestor(root,value1,value2){
+    function findLowestCommonAncestorHelper(root,value1,value2){
+        if(!root){
+            return;
+        }
+        if(Math.max(value1,value2)<root.value){
+            return findLowestCommonAncestorHelper(root.left,value1,value2);
+        }
+        if(Math.min(value1,value2)>root.value){
+            return findLowestCommonAncestorHelper(root.right,value1,value2);
+        }
+        return root.value;
+    }
+    return findLowestCommonAncestorHelper(root,value1,value2);
+}
+
+var node1 = {
+    value: 1,
+    left: {
+        value: 0
+    },
+    right: {
+        value: 2
+    }
+}
+
+var node2 = {
+    value: 1,
+    left: {
+        value: 0.
+        left: {
+        value: -1
+    },
+    right: {
+        value: 0.5
+    }
+    },
+    right: {
+        value: -2
+    }
+}
+
+console.log(findLowestCommonAncestor(node1,0,2));
+console.log(findLowestCommonAncestor(node2,0,2));
+console.log(findLowestCommonAncestor(node1,0.5,-1));
