0cc4384c62c18a76757e975d715fa5b27ebc7cf2
Shaheer-Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2019-09-18 03:19:48 +0500

    Completed functions

--- Graph/Directed graph.js
+++ Graph/Directed graph.js
@@ -13,6 +13,19 @@ DirectedGraph.prototype.addEdge=function(orgVertex,desVertex,weight){
     this.edges[orgVertex][desVertex]=weight;
 }
 
+DirectedGraph.prototype.removeEdge = function(orgVertex,destVertex){
+    if(this.edges[orgVertex] && this.edges[orgVertex][destVertex != undefined]){
+        delete this.edges[orgVertex][destVertex];
+    }
+}
+
+DirectedGraph.prototype.removeVertex = function(vertex){
+    for(var adjVertex in this.edges[vertex]){
+        this.removeEdge(adjVertex,vertex);
+    }
+    delete this.edges[vertex];
+}
+
 var dirgraph=new DirectedGraph();
 dirgraph.addVertex("A");
 dirgraph.addVertex("B");
